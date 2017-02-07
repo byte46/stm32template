027b9d4548ca479f749eee50cbe175dc52804c74
Serge AKA Byte <byte.kgd@gmail.com>
2017-02-07 18:56:52 +0300

    UART

--- src/main.c
+++ src/main.c
@@ -10,17 +10,17 @@ int main(int argc, char *argv[])
     USART_InitTypeDef USART_InitStructure;
     GPIO_InitTypeDef GPIO_InitStructure;
     volatile u32 delay;
-/* GPIOC Periph clock enable */
+    /* GPIOC Periph clock enable */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOA |RCC_APB2Periph_USART1, ENABLE);
-/* Configure PC12 to mode: slow rise-time, pushpull output */
+    /* Configure PC12 to mode: slow rise-time, pushpull output */
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13; // GPIO No. 12
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; // slow rise time
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz; // slow rise time
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // push-pull output
     GPIO_Init(GPIOC, &GPIO_InitStructure); // GPIOC init
 
     /* Configure USART1 Tx (PA9) as alternate function push-pull */ 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; 
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz; 
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; 
     GPIO_Init(GPIOA, &GPIO_InitStructure); 
  
