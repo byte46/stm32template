20a5a9e9c76a9a27850fa858c5d9303938440dfe
Serge AKA Byte <byte.kgd@gmail.com>
2017-02-19 23:51:10 +0300

    Таймеры, поиски глюков в TIM_ClearITPendingBit

--- src/main.c
+++ src/main.c
@@ -1,37 +1,55 @@
 #include "stm32f10x.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"
+#include "stm32f10x_tim.h"
 
 
-int main(void)
+volatile uint16_t previousState;
+GPIO_InitTypeDef port;
+TIM_TimeBaseInitTypeDef timer;
+
+int main()
 {
-    GPIO_InitTypeDef gp;
-    //RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    RCC->APB2ENR |= ((uint32_t)0x00000010); // GPIOC Enable
-    //RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-    RCC->APB1ENR |= ((uint32_t)0x00000001); // TIM2 Enable
-    gp.GPIO_Pin = GPIO_Pin_13;
-    gp.GPIO_Mode = GPIO_Mode_Out_PP;
-    gp.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &gp);
-    TIM2->PSC = 7200 - 1;
-    TIM2->ARR = 1;
-    TIM2->DIER |= TIM_DIER_UIE;
-    TIM2->CR1 |= TIM_CR1_CEN;
-    NVIC_EnableIRQ(TIM2_IRQn);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 
+
+    GPIO_StructInit(&port);
+    port.GPIO_Mode = GPIO_Mode_Out_PP;
+    port.GPIO_Pin = GPIO_Pin_13;
+    port.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &port);
+
+    TIM_TimeBaseStructInit(&timer);
+    timer.TIM_Prescaler = 7200-1;
+    timer.TIM_Period = 6000;
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    TIM_TimeBaseInit(TIM2, &timer);
+    TIM_Cmd(TIM2, ENABLE);
+    NVIC_EnableIRQ(TIM2_IRQn);
     while(1)
     {
-        //GPIOC->BSRR = GPIO_BSRR_BR13; // On
-        //GPIOC->BSRR = GPIO_BSRR_BS13; // Off
     }
-
 }
 
-void TIM2_IRQHandler(void)
+void TIM2_IRQHandler()
 {
-    TIM2->SR &= ~TIM_SR_UIF; 
-    //TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-    GPIOC->BSRR = GPIO_BSRR_BR13; // On
-    GPIOC->BSRR = GPIO_BSRR_BS13; // Off
+    if (previousState == 0)
+    {
+	previousState = 1;
+	GPIO_SetBits(GPIOC, GPIO_Pin_13);
+	//timer.TIM_Period = 100;
+	//TIM_TimeBaseInit(TIM2, &timer);
+	//TIM_ClearITPendingBit(TIM2, TIM_IT_Update); // НЕ ИСПОЛЬЗОВАТЬ ЭТО УБОЖЕСТВО!!!
+    TIM2->SR = ~(TIM_SR_UIF);
+    }
+    else
+    {
+	previousState = 0;
+	GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+	//timer.TIM_Period = 100;
+	//TIM_TimeBaseInit(TIM2, &timer);
+	//TIM_ClearITPendingBit(TIM2, TIM_IT_Update); // НЕ ИСПОЛЬЗОВАТЬ ЭТО УБОЖЕСТВО!!!
+    TIM2->SR = ~(TIM_SR_UIF);
+    }
 }
